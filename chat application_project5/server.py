835f26666b4cc3f2d8b6684d40c2b392a4f89437
codeby-Disha <4isha2anik1906@gmail.com>
2026-01-13 11:01:10 +0530

    Update server.py

--- chat application_project5/server.py
+++ chat application_project5/server.py
@@ -1,19 +1,19 @@
 import socket
 
 def start_server():
-    # 1. Create a socket object (AF_INET = IPv4, SOCK_STREAM = TCP)
+    
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     
-    # 2. Bind the socket to a specific address and port
+ 
     host = '127.0.0.1'  # Localhost
     port = 12345        # Any non-privileged port
     server_socket.bind((host, port))
     
-    # 3. Listen for incoming connections
+    
     server_socket.listen(1)
     print(f"Server started. Waiting for a connection on {host}:{port}...")
     
-    # 4. Accept a connection
+   
     conn, addr = server_socket.accept()
     print(f"Connected by: {addr}")
     
@@ -35,4 +35,5 @@ def start_server():
     server_socket.close()
 
 if __name__ == "__main__":
+
     start_server()
